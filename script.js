daca1e4ed9bd5a75f66a1de77bf4c7b8655f05ee
Vered Rekanati <veredrec@gmail.com>
2018-05-07 20:50:16 -0400

    Setup basic html js and css

---
+++ script.js
@@ -0,0 +1,61 @@
+// Set table and goal according to user input
+
+// 1. Get user input
+var number, goal;
+$('#submit').on('click', function() {
+  $('#submit').addClass('hide-element');
+  $('#submit').removeClass('remove-element');
+  number = $('#number').val();
+  goal = $('#goal').val();
+  if (number > 0) {
+    setTable(number);
+  }
+  if (goal.length > 0) {
+    setGoal(goal);
+  }
+});
+// 2. Set table
+function setTable(number) {
+  $('#table').remove(); // remove table if exists
+  $('#table-container').append('<table id="table">'); // create new table
+  var counter = 1;
+  var container;
+  for (var i = 0; i < number; i++) {
+    // create number of rows as num of weeks
+    container = $('<tr/>');
+    for (var x = 0; x < 7; x++) {
+      // create 7 days for each row/week
+      container.append(
+        "<td class='box'>Day " +
+          Number(x + counter) +
+          "<input id='dailyGoal' class='daily-goal' type='text' placeholder='Daily Goal' onkeydown='getInput(this)'></td>"
+      );
+      $('#table').append(container); // append week to table
+    }
+    counter += 7; // jump 7 days ahead (for counting Day #)
+  }
+  // show and hide table
+  $('#table').addClass('show-element');
+  $('#table').removeClass('hide-element');
+}
+
+// 3. Set goal
+function setGoal(goal) {
+  $('#goalTitle').text($('#goal').val() + ". I'm gonna nail it!");
+  $('#goalTitle').addClass('show-element');
+  $('#goalTitle').removeClass('hide-element');
+}
+
+// Show and hide submit button
+$('#number, #goal').on('click', function() {
+  $('#submit').addClass('show-element');
+  $('#submit').removeClass('hide-element');
+});
+
+// set daily goals
+function getInput(userInput) {
+  if (event.key === 'Enter') {
+    var goal = userInput.value;
+    console.log(this);
+  }
+}
